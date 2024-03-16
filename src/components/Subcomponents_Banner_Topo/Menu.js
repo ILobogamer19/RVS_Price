c93f3840893f26ca525d017968d75c2cbea2f61e
ILobogamer19 <manuelgornelasn@gmail.com>
2024-03-16 00:51:13 -0300

    Remoção de ferramenta no codigo

---
+++ src/components/Subcomponents_Banner_Topo/Menu.js
@@ -0,0 +1,15 @@
+export default function Menu() {
+  return (
+    <div className="Links_Navegacao">
+      <a href="">Categorias</a>
+      <p className="Separacao_Links_Navegacao">|</p>
+      <a href="">Ofertas do dia</a>
+      <p className="Separacao_Links_Navegacao">|</p>
+      <a href="">Histórico</a>
+      <p className="Separacao_Links_Navegacao">|</p>
+      <a href="">Vender</a>
+      <p className="Separacao_Links_Navegacao">|</p>
+      <a href="">Contato</a>
+    </div>
+  );
+}
---
+++ src/pages/Home/HomePage.js
@@ -0,0 +1,10 @@
+import "./style.css";
+import Banner_Topo from "../../components/Banner_Topo";
+
+export default function HomePage() {
+  return (
+    <div className="Corpo_Site">
+      <Banner_Topo />
+    </div>
+  );
+}
